506a94419a0f2b2b01128ddf6e3a9417a0483469
Beomseo Kim <bskim030427@naver.com>
2024-03-22 15:37:37 +0900

    Initial commit

--- W03_2022047810_김범서/W03_hw03.py
+++ W03_2022047810_김범서/W03_hw03.py
@@ -13,7 +13,12 @@ teachers = np.zeros(N)
 for i in range(N):
     # Calculate the number of teachers for each playground
     # Compare the case with and without homeroom teacher
-    teachers[i] = np.array([1 + np.ceil((M[i] - X) / Y), np.ceil(M[i] / Y)]).min()
+    if X > M[i]:
+        case1 = 1
+    else:
+        case1 = 1 + np.ceil((M[i] - X) / Y)
+    case2 = np.ceil(M[i] / Y)
+    teachers[i] = np.array([case1, case2]).min()
 
 # Print the results
 print(int(teachers.sum()))
