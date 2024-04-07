28c3452c32f35e0332867fbf0123a8715f2710b2
Beomseo Kim <bskim030427@naver.com>
2024-04-08 01:06:56 +0900

    Minor changes

--- W05/fitting.py
+++ W05/fitting.py
@@ -32,7 +32,7 @@ r_squared = rss / tss
 
 # print("R^2 =", r_squared)
 
-plt.figure(figsize=(12, 6))
+plt.figure(figsize=(14, 6))
 
 # 측정 데이터 표시
 plt.scatter(vol, np.abs(cur))
@@ -42,10 +42,10 @@ plt.plot(vol, np.abs(predicted_cur1), label=f"R$^2$ = {r_squared}", color="red")
 
 # -2V, -1V, 1V 지점에 전류값 표시
 for point in [-2, -1, 1]:
-    plt.text(vol[i := np.where(vol == point)[0][0]], y := np.abs(cur[i]), f"{y:.4e}", ha="center", va="bottom")
+    plt.text(vol[i := np.where(vol == point)[0][0]], y := np.abs(cur[i]), f"{y:.4e}", ha="center", va="bottom", fontsize="large")
 
 plt.legend(loc="center left")
-plt.yticks([1e-11, 1e-10, 1e-9, 1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3])
+
 plt.title("I-V Raw data + Fitted data")
 plt.xlabel("Voltage (V)")
 plt.ylabel("Current (A)")
