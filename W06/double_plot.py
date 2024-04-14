d99c1f7746feee5e449c25acc12c4f2d80eecf8a
Beomseo Kim <bskim030427@naver.com>
2024-04-14 23:01:20 +0900

    Configured model and parameters

--- W06/double_plot.py
+++ W06/double_plot.py
@@ -11,23 +11,29 @@ soup = BeautifulSoup(xml_data, "xml")
 IVMeasurement = soup.find("IVMeasurement")
 vol = np.array([float(v) for v in IVMeasurement.find("Voltage").text.split(",")])
 cur = np.array([float(c) for c in IVMeasurement.find("Current").text.split(",")])
-vol[8] += 1e-3
 
 
-def diode_model(v, isat, n):
-    return isat * (np.exp(v / (26e-3 * n)) - 1)
+def diode_model(v, isat, vt, n, p, q):
+    return isat * (np.exp((v + p) / (vt * n)) - 1) + q
+    # return isat * (np.exp(v / (vt * n)) - 1)
 
 
 model = Model(diode_model)
-params = model.make_params(isat=-1.06e-8, n=1)
+params = model.make_params(isat=0, vt=26e-3, n=1, p=0, q=0)
 params['n'].min = 1
 result = model.fit(cur, v=vol, params=params)
 print(result.fit_report())
 print(result.params)
+
+x_new = np.linspace(-2, 1, 1000)
+#params_new = [-2.2811e-18, 0.03410632, 1, 0.7, 4.6117e-09]
+params_new = result.params.values()
+y_pred = diode_model(x_new, *params_new)
+
 plt.figure(figsize=(12, 6))
 plt.subplot(1, 2, 1)
 plt.scatter(vol, np.abs(cur))
-plt.plot(x := np.linspace(-2, 1, 100), np.abs(diode_model(x, *result.params.values())), label='Fitted data',
+plt.plot(x_new, np.abs(np.abs(y_pred)), label='Fitted data',
          color='red')
 plt.title("I-V Measured data")
 plt.yscale('log')
