e54415dd74b1fe977687bb83eb35b9cb178c2f41
bskim <kbs03@hanyang.ac.kr>
2024-04-15 12:35:15 +0900

    Configured diode model and plot appearance

--- W06/double_plot.py
+++ W06/double_plot.py
@@ -1,9 +1,10 @@
 from bs4 import BeautifulSoup
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 from lmfit import Model
 
-with open("../HY202103_D07_(0,0)_LION1_DCM_LMZC.xml", "r") as f:
+with open("..\HY202103_D07_(0,0)_LION1_DCM_LMZC.xml", "r") as f:
     xml_data = f.read()
 
 soup = BeautifulSoup(xml_data, "xml")
@@ -13,34 +14,53 @@ vol = np.array([float(v) for v in IVMeasurement.find("Voltage").text.split(",")]
 cur = np.array([float(c) for c in IVMeasurement.find("Current").text.split(",")])
 
 
-def diode_model(v, isat, vt, n, p, q):
-    return isat * (np.exp((v + p) / (vt * n)) - 1) + q
-    # return isat * (np.exp(v / (vt * n)) - 1)
+def diode_model(voltage, isat, vt, n):
+    # isat: saturation current
+    # vt: thermal voltage
+    # n: ideality factor
+    return isat * (np.exp(voltage / (vt * n)) - 1)
 
 
-model = Model(diode_model)
-params = model.make_params(isat=0, vt=26e-3, n=1, p=0, q=0)
+model_pos = Model(diode_model)
+params = model_pos.make_params(isat=1e-5, vt=26e-3, n=1)
 params['n'].min = 1
-result = model.fit(cur, v=vol, params=params)
-print(result.fit_report())
-print(result.params)
+result_pos = model_pos.fit(cur[8:], voltage=vol[8:], params=params)
+# print(result.fit_report())
+# print(result.params)
+model_neg = Model(diode_model)
+params = model_neg.make_params(isat=0, vt=26e-3, n=1)
+params['n'].min = 1
+result_neg = model_neg.fit(cur[:8], voltage=vol[:8], params=params)
 
 x_new = np.linspace(-2, 1, 1000)
-#params_new = [-2.2811e-18, 0.03410632, 1, 0.7, 4.6117e-09]
-params_new = result.params.values()
-y_pred = diode_model(x_new, *params_new)
-
-plt.figure(figsize=(12, 6))
-plt.subplot(1, 2, 1)
-plt.scatter(vol, np.abs(cur))
-plt.plot(x_new, np.abs(np.abs(y_pred)), label='Fitted data',
+#x_new[666] += 1e-4
+params_new_pos = result_pos.params.values()
+params_new_neg = result_neg.params.values()
+cur_pred_continuous_pos = diode_model(x_new_pos := x_new[667:], *params_new_pos)
+cur_pred_continuous_neg = diode_model(x_new_neg := x_new[:667], *params_new_neg)
+print(cur_pred_continuous_pos[0])
+fig, (axIV, axTrans) = plt.subplots(1, 2)
+fig.set_size_inches(16, 9)
+axIV.scatter(vol, np.abs(cur), label="Measured")
+axIV.plot(x_new_pos, np.abs(np.abs(cur_pred_continuous_pos)), label='Fitted',
          color='red')
-plt.title("I-V Measured data")
-plt.yscale('log')
-plt.xlabel('Voltage [V]')
-plt.ylabel('Current [A]')
-plt.tick_params(axis='x', direction='in')
-plt.tick_params(axis='y', direction='in')
+axIV.plot(x_new_neg, np.abs(np.abs(cur_pred_continuous_neg)), label='Fitted',
+         color='red')
+axIV.set_title("I-V Measured data + Fitted data")
+axIV.set_yscale('log')
+axIV.yaxis.set_major_locator(ticker.LogLocator(base=10, numticks=12))
+axIV.yaxis.set_minor_locator(ticker.LogLocator(base=10, subs=(0.0, 0.2, 0.4, 0.6, 0.8), numticks=12))
+axIV.yaxis.set_minor_formatter(ticker.NullFormatter())
+axIV.tick_params(axis='both', direction='in', which='both')
+axIV.set_xlabel('Voltage [V]')
+axIV.set_ylabel('Log Absolute Current [A]')
+axIV.legend(['Measured', f'Fitted'])
+# -2V, -1V, 1V 지점에 전류값 표시
+#for point in [-2, -1, 1]:
+#    index = np.where(vol == point)[0][0]
+#    x = vol[index]
+#    y = np.abs(cur[index]) * 1.3
+#    axIV.text(x, y, f"{y / 1.3:.4e}", ha="center", va="bottom", fontsize="large")
 
 WavelengthSweep = soup.find_all("WavelengthSweep")
 l_list = []
@@ -52,14 +72,12 @@ for ws in WavelengthSweep:
     il_list.append([float(_il) for _il in ws.find("IL").text.split(",")])
 l = np.array(l_list)
 il = np.array(il_list)
-plt.subplot(1, 2, 2)
 for i in range(len(l) - 1):
-    plt.plot(l[i], il[i], label=vol_list[i])
-plt.plot(l[-1], il[-1])
-plt.title("Transmission spectra")
-plt.legend(ncol=3, loc='lower center')
-plt.xlabel('Wavelength [nm]')
-plt.ylabel('Measured transmission [dB]')
-plt.tick_params(axis='x', direction='in')
-plt.tick_params(axis='y', direction='in')
+    axTrans.plot(l[i], il[i], label=vol_list[i])
+axTrans.plot(l[-1], il[-1])
+axTrans.set_title("Transmission spectra")
+axTrans.legend(ncol=3, loc='lower center')
+axTrans.set_xlabel('Wavelength [nm]')
+axTrans.set_ylabel('Measured transmission [dB]')
+axTrans.tick_params(axis='both', direction='in')
 plt.show()
